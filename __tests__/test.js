5c1351b47681e43fdccf9e5045c1c68690c46e02
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-05 19:30:21 +0400

    del second tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,27 +1,14 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-
-test('Test N1.json', () => {
-  const expectedN1 = fs.readFileSync('./__tests__/__fixtures__/test1_result', 'utf8');
-  const resultN1 = genDiff('./__tests__/__fixtures__/test1_before.json', './__tests__/__fixtures__/test1_after.json');
-  expect(resultN1).toBe(expectedN1);
-});
-
-test('Test N1.yml', () => {
-  const expectedN1 = fs.readFileSync('./__tests__/__fixtures__/test1_result', 'utf8');
-  const resultN1 = genDiff('./__tests__/__fixtures__/test1_before.yml', './__tests__/__fixtures__/test1_after.yml');
-  expect(resultN1).toBe(expectedN1);
-});
-
-test('Test N2.json', () => {
-  const expectedN2 = fs.readFileSync('./__tests__/__fixtures__/test2_result', 'utf8');
-  const resultN2 = genDiff('./__tests__/__fixtures__/test2_before.json', './__tests__/__fixtures__/test2_after.json');
-  expect(resultN2).toBe(expectedN2);
-});
-
-test('Test N2.yml', () => {
-  const expectedN2 = fs.readFileSync('./__tests__/__fixtures__/test2_result', 'utf8');
-  const resultN2 = genDiff('./__tests__/__fixtures__/test2_before.yml', './__tests__/__fixtures__/test2_after.yml');
-  expect(resultN2).toBe(expectedN2);
+describe('gendiff', () => {
+  const expected = fs.readFileSync('./__tests__/__fixtures__/test1_result', 'utf8');
+  it('Test N1.json', () => {
+    const result = genDiff('./__tests__/__fixtures__/test1_before.json', './__tests__/__fixtures__/test1_after.json');
+    expect(result).toBe(expected);
+  });
+  it('Test N1.yml', () => {
+    const result = genDiff('./__tests__/__fixtures__/test1_before.yml', './__tests__/__fixtures__/test1_after.yml');
+    expect(result).toBe(expected);
+  });
 });
