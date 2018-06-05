c0f624eda6fddca7695f165aad64c1b5441a0d30
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-05 14:30:15 +0400

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
 import fs from 'fs';
+import _ from 'lodash';
+import yaml from 'js-yaml';
+import path from 'path';
 
-const parseFile = pathToFile => JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
-
-export default (pathToFile1, pathToFile2) => {
-  const [file1, file2] = [parseFile(pathToFile1), parseFile(pathToFile2)];
-  const [keysFile1, keysFile2] = [Object.keys(file1), Object.keys(file2)];
-
-  const addedKeys = keysFile2.filter(key => !keysFile1.includes(key));
-  const deletedKeys = keysFile1.filter(key => !keysFile2.includes(key));
-  const equalKeys = keysFile2.filter(key => keysFile1.includes(key) && file1[key] === file2[key]);
-  const changedKeys = keysFile2.filter(key => keysFile1.includes(key) && file1[key] !== file2[key]);
-
-
-  const keysActions = [
+const adapter = (pathToFile) => {
+  const fileExt = path.extname(pathToFile);
+  const extActions = [
     {
-      action: key => `  + ${key}: ${file2[key]}`,
-      check: key => addedKeys.includes(key),
+      extName: '.json',
+      action: () => JSON.parse(fs.readFileSync(pathToFile, 'utf8')),
     },
     {
-      action: key => `  - ${key}: ${file1[key]}`,
-      check: key => deletedKeys.includes(key),
-    },
-    {
-      action: key => `    ${key}: ${file2[key]}`,
-      check: key => equalKeys.includes(key),
-    },
-    {
-      action: key => [`  + ${key}: ${file2[key]}`, `  - ${key}: ${file1[key]}`],
-      check: key => changedKeys.includes(key),
+      extName: '.yml',
+      action: () => yaml.safeLoad(fs.readFileSync(pathToFile, 'utf8')),
     },
   ];
+  const getExtActions = fileExtName => extActions.find(({ extName }) => fileExtName === extName);
+  return getExtActions(fileExt).action();
+};
 
-  const getKeyAction = key => keysActions.find(({ check }) => check(key));
-
-  const result = [...equalKeys, ...changedKeys, ...deletedKeys, ...addedKeys].reduce((acc, key) => {
-    const outputStr = getKeyAction(key).action(key);
-    return Array.isArray(outputStr) ? [...acc, ...outputStr] : [...acc, outputStr];
-  }, []);
-  return `{\n${result.join('\n')}\n}\n`;
+export default (pathToFile1, pathToFile2) => {
+  const file1 = adapter(pathToFile1);
+  const file2 = adapter(pathToFile2);
+  const filesKeys = _.union(Object.keys(file1), Object.keys(file2));
+  const result = filesKeys.map((key) => {
+    if (!_.has(file1, key)) {
+      return `  + ${key}: ${file2[key]}`;
+    }
+    if (!_.has(file2, key)) {
+      return `  - ${key}: ${file1[key]}`;
+    }
+    if (_.has(file1, key) && file1[key] !== file2[key]) {
+      return [`  + ${key}: ${file2[key]}`, `  - ${key}: ${file1[key]}`];
+    }
+    return `    ${key}: ${file2[key]}`;
+  });
+  return `{\n${_.flatten(result).join('\n')}\n}\n`;
 };
