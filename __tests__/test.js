a2ad5391787e827e153ac3ca2ff3d89502d02214
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-06 21:20:50 +0400

    add .ini test

--- __tests__/test.js
+++ __tests__/test.js
@@ -3,12 +3,16 @@ import genDiff from '../src';
 
 describe('gendiff', () => {
   const expected = fs.readFileSync('./__tests__/__fixtures__/test1_result', 'utf8');
-  it('Test N1.json', () => {
+  it('Test N1 for ".json"', () => {
     const result = genDiff('./__tests__/__fixtures__/test1_before.json', './__tests__/__fixtures__/test1_after.json');
     expect(result).toBe(expected);
   });
-  it('Test N1.yml', () => {
+  it('Test N2 for ".yml"', () => {
     const result = genDiff('./__tests__/__fixtures__/test1_before.yml', './__tests__/__fixtures__/test1_after.yml');
+    expect(result).toBe(expected);
+  });
+  it('Test N3 for ".ini"', () => {
+    const result = genDiff('./__tests__/__fixtures__/test1_before.ini', './__tests__/__fixtures__/test1_after.ini');
     expect(result).toBe(expected);
   });
 });
