98f59124b2f0ec6b9246b901cb3eae82e4a45b59
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-06 21:21:14 +0400

    fix version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('0.1.3')
+  .version('0.1.4')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format')
   .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)));
