24c246ca64344bb326b94a7732b330eefa1505ac
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-06 21:20:01 +0400

    add .ini parser

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import fs from 'fs';
 import _ from 'lodash';
 import yaml from 'js-yaml';
 import path from 'path';
+import ini from 'ini';
 
 const parsers = {
   '.json': JSON.parse,
   '.yaml': yaml.safeLoad,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 const getParser = format => (data) => {
@@ -30,20 +32,20 @@ const parseData = (dataObj) => {
 };
 
 export default (pathToFile1, pathToFile2) => {
-  const file1 = parseData(readData(pathToFile1));
-  const file2 = parseData(readData(pathToFile2));
-  const filesKeys = _.union(Object.keys(file1), Object.keys(file2));
+  const parsedData1 = parseData(readData(pathToFile1));
+  const parsedData2 = parseData(readData(pathToFile2));
+  const filesKeys = _.union(Object.keys(parsedData1), Object.keys(parsedData2));
   const result = filesKeys.map((key) => {
-    if (!_.has(file1, key)) {
-      return `  + ${key}: ${file2[key]}`;
+    if (!_.has(parsedData1, key)) {
+      return `  + ${key}: ${parsedData2[key]}`;
     }
-    if (!_.has(file2, key)) {
-      return `  - ${key}: ${file1[key]}`;
+    if (!_.has(parsedData2, key)) {
+      return `  - ${key}: ${parsedData1[key]}`;
     }
-    if (_.has(file1, key) && file1[key] !== file2[key]) {
-      return [`  + ${key}: ${file2[key]}`, `  - ${key}: ${file1[key]}`];
+    if (_.has(parsedData1, key) && parsedData1[key] !== parsedData2[key]) {
+      return [`  + ${key}: ${parsedData2[key]}`, `  - ${key}: ${parsedData1[key]}`];
     }
-    return `    ${key}: ${file2[key]}`;
+    return `    ${key}: ${parsedData2[key]}`;
   });
   return `{\n${_.flatten(result).join('\n')}\n}\n`;
 };
