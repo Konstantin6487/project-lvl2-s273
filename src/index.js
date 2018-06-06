aa5f33628e8b0437c11e4e0cb87a6b95fa61df30
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-06 13:07:14 +0400

    fix parser fn

--- src/index.js
+++ src/index.js
@@ -3,25 +3,37 @@ import _ from 'lodash';
 import yaml from 'js-yaml';
 import path from 'path';
 
-const adapter = (pathToFile) => {
-  const fileExt = path.extname(pathToFile);
-  const extActions = [
-    {
-      extName: '.json',
-      action: () => JSON.parse(fs.readFileSync(pathToFile, 'utf8')),
-    },
-    {
-      extName: '.yml',
-      action: () => yaml.safeLoad(fs.readFileSync(pathToFile, 'utf8')),
-    },
-  ];
-  const getExtActions = fileExtName => extActions.find(({ extName }) => fileExtName === extName);
-  return getExtActions(fileExt).action();
+const parsers = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.yml': yaml.safeLoad,
+};
+
+const getParser = format => (data) => {
+  const parse = parsers[format];
+  if (!parse) {
+    throw new Error(`unkown format: ${format}`);
+  }
+  return parse(data);
+};
+
+const readParseData = (pathToFile) => {
+  if (!pathToFile) {
+    return '';
+  }
+  const format = path.extname(pathToFile);
+  const data = fs.readFileSync(pathToFile, 'utf8');
+  const parse = getParser(format);
+  const config = parse(data);
+  return config;
 };
 
 export default (pathToFile1, pathToFile2) => {
-  const file1 = adapter(pathToFile1);
-  const file2 = adapter(pathToFile2);
+  if (!pathToFile1 && !pathToFile2) {
+    return 'Files not found';
+  }
+  const file1 = readParseData(pathToFile1) || {};
+  const file2 = readParseData(pathToFile2) || {};
   const filesKeys = _.union(Object.keys(file1), Object.keys(file2));
   const result = filesKeys.map((key) => {
     if (!_.has(file1, key)) {
