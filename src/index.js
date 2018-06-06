4c5dca916a0f9e786a69ef0ec4bc84b5bef8d902
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-06 13:15:32 +0400

    fix errors

--- src/index.js
+++ src/index.js
@@ -18,13 +18,10 @@ const getParser = format => (data) => {
 };
 
 const readParseData = (pathToFile) => {
-  if (!pathToFile) {
-    return '';
-  }
   const format = path.extname(pathToFile);
   const data = fs.readFileSync(pathToFile, 'utf8');
   const parse = getParser(format);
-  const config = parse(data);
+  const config = parse(data) || {};
   return config;
 };
 
@@ -32,8 +29,8 @@ export default (pathToFile1, pathToFile2) => {
   if (!pathToFile1 && !pathToFile2) {
     return 'Files not found';
   }
-  const file1 = readParseData(pathToFile1) || {};
-  const file2 = readParseData(pathToFile2) || {};
+  const file1 = readParseData(pathToFile1);
+  const file2 = readParseData(pathToFile2);
   const filesKeys = _.union(Object.keys(file1), Object.keys(file2));
   const result = filesKeys.map((key) => {
     if (!_.has(file1, key)) {
