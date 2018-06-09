82a82bd2cefe547806ad3408e08417db019a53a1
Konstantin6487 <konstantin6487@rambler.ru>
2018-06-10 03:20:50 +0400

    update

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,10 +4,11 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('0.2.4')
+  .version('0.2.5')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Outputs (json, yml/yaml, ini)')
-  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)));
+  .action((firstConfig, secondConfig) =>
+    console.log(genDiff(firstConfig, secondConfig, program.format)));
 
 program
   .description('Compares two configuration files and shows a difference.');
